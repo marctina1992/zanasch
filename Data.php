fba8ef552d1576edbb9af03efcdcecd7972e1657
Twixmaximus <134007646+marctina1992@users.noreply.github.com>
2025-03-30 11:52:40 +0100

    Initial commit

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'root'; 
+$DatabaseName = 'Zanaschool1_Test'; 
+$DatabasePort = '3306'; 
+?>
